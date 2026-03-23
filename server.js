9fb397a56efec2d3101a64740138f695788f1b82
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-03-23 20:09:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const path = require('path');
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
 const app = express();
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
-// Test route
+
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("Pesa Grow Backend Running ✅");
+  res.send("Server is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // ====================== CONFIG ======================
