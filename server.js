64a3d451285ecbf8d2aece6f3d67304186dc5b95
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-03-23 19:55:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Pesa Grow Backend Running ✅");
+});
 
 // ====================== CONFIG ======================
 const CONSUMER_KEY = process.env.MPESA_CONSUMER_KEY;
