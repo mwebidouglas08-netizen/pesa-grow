6521647300855562630eb35e0a3addf32d423be2
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-03-25 08:42:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const path = require("path");
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+// Explicit routes (important for Railway)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "admin.html"));
+});
 require("dotenv").config();
 
 const express = require("express");
