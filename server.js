eaa4a1d4d077853a10a70a45e53f1fee0695fb55
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-03-25 07:40:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Pesa Grow API running...");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
